c364f36fa59439889bf88f1fbdaad8e1687d721a
Noel Mbuya <mbuyanoel@gmail.com>
2019-10-16 22:10:16 +0300

    Added the initial html and scripts files

---
+++ script.js
@@ -0,0 +1,13 @@
+//"use strict"
+
+const video = document.getElementById('video');
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} }, 
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+startVideo();
